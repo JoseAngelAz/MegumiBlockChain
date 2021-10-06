0e618ddf8167fa5eb70ba71b3315d12034dd1038
José Angel Azucena <angelvanitas555@gmail.com>
2021-10-05 22:30:23 -0600

    corrigiendo errores en test con jest

--- test.js
+++ test.js
@@ -11,10 +11,10 @@ describe('block',()=>{
     });
 //test 1: meter datos en el bloque para comparar el input
     it('Set the data in block to match the input', ()=>{
-        expect(block.data).toEqueal(data);
+        expect(block.data).toEqual(data);
     });
 //test 2: meter el ultimo hash en el block para comparar el hash de el ultimo bloque
     it('Set the lastHash in block to match the hash of the last Block', ()=>{
-        expect(block.lastHash).toEqueal(lastBlock.hash);
+        expect(block.lastHash).toEqual(lastBlock.hash);
     });
 });
