6653e5c115a99440fb6c1dadb74e3e8507ede34e
José Angel Azucena <angelvanitas555@gmail.com>
2021-10-05 21:43:47 -0600

    Revert "cambiado"
    This reverts commit b45d180d8155385158797e12c39cff06a9711fb7.

--- devTesting.js
+++
@@ -1,7 +0,0 @@
-const Block = require('./block');
-
-const genesis = Block.genesis();
-const fooBlock = Block.mineBlock(genesis, []);
-
-console.log(genesis.toString());
-console.log(fooBlock.toString());
