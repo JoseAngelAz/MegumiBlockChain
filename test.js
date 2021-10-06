45a8b2ce9751309be5249e362870d9844e7f5931
José Angel Azucena <angelvanitas555@gmail.com>
2021-10-05 22:27:57 -0600

    comentando codigo de pruebas de jest

--- test.js
+++ test.js
@@ -9,10 +9,11 @@ describe('block',()=>{
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
     });
-
+//test 1: meter datos en el bloque para comparar el input
     it('Set the data in block to match the input', ()=>{
         expect(block.data).toEqueal(data);
     });
+//test 2: meter el ultimo hash en el block para comparar el hash de el ultimo bloque
     it('Set the lastHash in block to match the hash of the last Block', ()=>{
         expect(block.lastHash).toEqueal(lastBlock.hash);
     });
